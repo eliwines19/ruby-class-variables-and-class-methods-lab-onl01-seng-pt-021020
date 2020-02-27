5a0e59d1ce205d8cd0ec3541c438d6ff413cb785
Elijah wines <elijahwines84@yahoo.com>
2020-02-27 20:21:28 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+
+  attr_accessor :name, :artists, :genre
+
+  @@songs = 0
+
+  def initialize(name)
+    @name = name
+    @@songs += 1
+  end
+  
+  def self.count
+    @@songs
+  end
+  
+end
