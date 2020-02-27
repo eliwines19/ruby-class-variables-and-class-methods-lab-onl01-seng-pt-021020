3ad5765ae133a83bcd5a88b939e2741428b6c78e
Elijah wines <elijahwines84@yahoo.com>
2020-02-27 20:28:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,10 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artists = []
-  @@genres = []
 
   def initialize(name, artist, genre)
     @@count += 1
+    @@artists = []
     @name = name
     @artist = artist
     @genre = genre
